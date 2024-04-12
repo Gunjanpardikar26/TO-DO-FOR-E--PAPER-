b0d9bb336917585bb9b19d5662ac6b84d46cd98e
Gunjanpardikar26 <112976952+Gunjanpardikar26@users.noreply.github.com>
2024-04-12 19:29:31 +0530

    Update index.js

--- Server/index.js
+++ Server/index.js
@@ -15,6 +15,10 @@ app.use(express.json())
 
 mongoose.connect('mongodb://127.0.0.1:27017/test')
 
+app.get("/",(req,res) => {
+    res.json("hello");
+})
+
 app.get('/get', (req,res ) =>{
     TodoModel.find()
     .then(result =>res.json(result))
