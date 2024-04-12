e2a039564c9f6c911748542b495331a65bb5136a
Gunjanpardikar26 <112976952+Gunjanpardikar26@users.noreply.github.com>
2024-04-12 19:37:43 +0530

    Update Create.jsx

--- todolist/src/Create.jsx
+++ todolist/src/Create.jsx
@@ -11,12 +11,12 @@ function Create({onTaskAdded,fetchTaskCount, count}) {
     const handleAdd = () => {
       fetchTaskCount();
       if (count >= 5) {
-        axios.get('to-do-for-e-paper.vercel.app
+        axios.get('https://to-do-for-e-paper.vercel.app
 /tasks/first')
           .then(response => {
             const firstTask = response.data;
             if (firstTask && firstTask.done) {
-              axios.put(`to-do-for-e-paper.vercel.app
+              axios.put(`https://to-do-for-e-paper.vercel.app
 /replace/${firstTask._id}`, { task: task })
                 .then(() => {
                   setTask('');
@@ -33,7 +33,7 @@ function Create({onTaskAdded,fetchTaskCount, count}) {
           })
           .catch(err => console.log("error while running first api"+err));
       } else {
-        axios.post('to-do-for-e-paper.vercel.app
+        axios.post('https://to-do-for-e-paper.vercel.app
 /add', { task: task })
           .then(() => {
             setTask('');
